8adc2c8606f680c9fc9e0e90731e9a8d7bebf17c
Kevin <kevinm1776@gmail.com>
2019-10-17 09:07:27 -0700

    Setup mongo env and heroku init.

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, url_for
+from pymongo import MongoClient
+
+
+client = MongoClient(
+    f'mongodb+srv://{os.getenv("MONGODB_USER")}:{os.getenv("MONGODB_PASSWORD")}@'
+    'cluster0-gwjng.mongodb.net/admin?retryWrites=true&w=majority', connect=False
+)
+
+db = client.Scheduler
+classtimes = db.classtimes
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return 'Hey world.'
